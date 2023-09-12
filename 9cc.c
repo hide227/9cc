6d4f37f09695cb29f843a1f6a77da5b8a5dc36d7
hide227 <n344219hondah@gmail.com>
2023-09-12 11:31:04 +0900

    update error messages

--- 9cc.c
+++ 9cc.c
@@ -25,10 +25,17 @@ struct Token {
 // current token
 Token *token;
 
+char *user_input;
+
 // reports an error and exit
-void error(char *fmt, ...) {
+void error_at(char *loc, char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " ");
+  fprintf(stderr, "^ ");  
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
   exit(1);
@@ -44,13 +51,13 @@ bool consume(char op) {
 
 void expect(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op)
-    error("'the next token is not %c", op);
+    error_at(token->str, "'the next token is not %c", op);
   token = token->next;
 }
 
 int expect_number() {
   if (token->kind != TK_NUM)
-    error("not integer");
+    error_at(token->str, "not integer");
   int val = token->val;
   token = token->next;
   return val;
@@ -91,7 +98,7 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    error("invalid token");
+    error_at(token->str, "invalid token");
   }
 
   new_token(TK_EOF, cur, p);
@@ -105,8 +112,10 @@ int main(int argc, char **argv)
     return 1;
   }
 
+  user_input = argv[1];
+
   // tokenize
-  token = tokenize(argv[1]);
+  token = tokenize(user_input);
   
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
