12dd9d2108b187d5f656ff79bae9a5955327873a
hide227 <n344219hondah@gmail.com>
2023-09-20 17:11:27 +0900

    add parser

--- 9cc.c
+++ 9cc.c
@@ -22,6 +22,25 @@ struct Token {
   char *str;      // token literal
 };
 
+// kind of AST node
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // integer
+} NodeKind;
+
+typedef struct Node Node;
+
+// type of AST node
+struct Node {
+  NodeKind kind; // type of node
+  Node *lhs;     // left-hand side
+  Node *rhs;     // right-hand side
+  int val;       // use if kind if ND_NUM
+};
+
 // current token
 Token *token;
 
@@ -104,6 +123,61 @@ Token *tokenize() {
 
   new_token(TK_EOF, cur, p);
   return head.next;
+}
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+Node *expr() {
+  Node *node = mul();
+
+  for(;;) {
+    if (consume('+'))
+      node = new_node(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_node(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+Node *mul() {
+  Node *node = primary();
+
+  for(;;) {
+    if (consume('*'))
+      node = new_node(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_node(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+Node *primary() {
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+
+  return new_node_num(expect_number());
 }
 
 int main(int argc, char **argv)
